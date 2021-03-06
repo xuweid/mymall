35168cc95f561074012b8c4c0abc22cd3d8342f1
xuweidong <2224908231@qq.com>
2021-03-06 16:17:22 +0800

    使用脚手架创建项目

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="tab-bar-item" @click="itemClick">
+		<div v-if="!isActive" class="item-img"><slot name="item-icon"></slot></div>
+		<div v-else class="item-img"><slot name="item-icon-active"></slot></div>
+		<div :style="activeStyle"><slot name="item-text"></slot></div>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+	  props:{
+      path:String,
+      activeColor:{
+        type:String,
+	      default:'pink'
+      }
+	  },
+	  computed:{
+      isActive(){
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+		  activeStyle(){
+        return this.isActive?{color:this.activeColor}:{}
+		  }
+	  },
+	  methods:{
+      itemClick(){
+        console.log('哈哈哈哈，点击了吧')
+	      this.$router.replace(this.path)
+      }
+	  }
+  }
+</script>
+
+<style>
+	.tab-bar-item {
+		flex: 1;
+		text-align: center;
+		height: 49px;
+		font-size: 14px;
+	}
+
+	.item-img img{
+		width: 24px;
+		height: 24px;
+		margin-top: 3px;
+		margin-bottom: 2px;
+		vertical-align: middle;
+	}
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>
+        <span>购物街</span>
+      </template>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+  export default {
+    name: "Home",
+    components: {NavBar}
+  }
+</script>
+
+<style scoped>
+
+.home-nav{
+  background-color: var(--color-tint);
+  color: #fff;
+}
+
+</style>
